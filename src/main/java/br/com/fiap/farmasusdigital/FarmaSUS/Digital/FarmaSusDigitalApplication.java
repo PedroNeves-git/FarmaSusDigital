fbbc6d5dc098755b46ec6286bed23c5721f0d5a4
Pedro Neves <sgtpedrocos@gmail.com>
2026-09-01 20:28:03 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/farmasusdigital/FarmaSUS/Digital/FarmaSusDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.farmasusdigital.FarmaSUS.Digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaSusDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaSusDigitalApplication.class, args);
+	}
+
+}
